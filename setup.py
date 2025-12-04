49f496478083ca8a500a14905a9897326fca9082
Sito Shinnosuke <sitsnk@icloud.com>
2025-12-04 16:40:25 +0900

    fix setup.py Dec4

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-import os
-from glob import glob
 from setuptools import setup
 
 package_name = 'mypkg'
+
+import os
+from glob import glob
 
 setup(
     name=package_name,
@@ -12,19 +13,21 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+        # ← これが launch ファイルをインストールする行
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='sitsnk',
     maintainer_email='sitsnk@icloud.com',
     description='ROS2 lesson10 test package',
-    license='GPL 3.0 only',
+    license='TODO: License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'talker = mypkg.talker:main',
-             'listener = mypkg.listener:main',
+            'listener = mypkg.listener:main',
         ],
     },
 )
+
