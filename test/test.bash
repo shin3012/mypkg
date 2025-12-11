7c7ad59f0cd2e832b8f72de76e22fd6b5046f05e
Sito Shinnosuke <sitsnk@icloud.com>
2025-12-11 16:04:04 +0900

    alarm for task2

--- test/test.bash
+++ test/test.bash
@@ -1,13 +1,24 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2025 Saito Shinnosuke
+# SPDX-License-Identifier: BSD-3-Clause
+
 
 dir=~
-[ "$1" != "" ] && dir="$1"
+[ "$1" != "" ] && dir="$1"   
 
-cd $dir/ros2_ws
+cd "$dir/ros2_ws"
 colcon build
-source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+
+source "$dir/.bashrc"
+
+timeout 10 ros2 launch mypkg talk_alarm.launch.py > /tmp/mypkg.log || true
+
+if grep -q 'ALERT' /tmp/mypkg.log; then
+    
+    exit 0
+else
+    echo 'ALERT not found in log'
+    exit 1
+fi
 
