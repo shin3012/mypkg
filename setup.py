68f6d95bb1e2e29b8717810b6ea6ec271b0fec1a
Sito Shinnosuke <sitsnk@icloud.com>
2025-12-11 16:08:40 +0900

    alarm for task2

--- setup.py
+++ setup.py
@@ -2,32 +2,27 @@ from setuptools import setup
 
 package_name = 'mypkg'
 
-import os
-from glob import glob
-
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # ← これが launch ファイルをインストールする行
-        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        ('share/' + package_name, ['launch/talk_alarm.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='sitsnk',
     maintainer_email='sitsnk@icloud.com',
-    description='ROS2 lesson10 test package',
-    license='TODO: License',
+    description='ROS2 package for alarm',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'talker = mypkg.talker:main',
-            'listener = mypkg.listener:main',
+            'threshold_alarm = mypkg.threshold_alarm:main',
         ],
     },
 )
-
